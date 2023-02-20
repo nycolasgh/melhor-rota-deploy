eafaa7770933697743031618bd2cb39adce46d1e
nycolasgh <nycolashartkopf@gmail.com>
2023-02-20 18:56:52 -0300

    deploy commit

--- staticfiles/static-root/js/google_places.js
+++ staticfiles/static-root/js/google_places.js
@@ -1,4 +1,5 @@
-$.getScript("https://maps.googleapis.com/maps/api/js?key=" + google_api_key + "&libraries=places&callback=initMap")
+
+$.getScript( "https://maps.googleapis.com/maps/api/js?key=" + google_api_key + "&libraries=places&callback=Function.prototype")
 .done(function( script, textStatus ) {
     google.maps.event.addDomListener(window, "load", initAutocomplete())
     google.maps.event.addDomListener(window, "load", initMap())
@@ -17,7 +18,7 @@ const options = {
    bounds: defaultBounds,
    componentRestrictions: {'country': ['br']},
    strictBounds: true,
-};
+}
 
 
 function initAutocomplete() {
@@ -33,7 +34,7 @@ function initAutocomplete() {
         var input_w = waypoints[i];
         var autocomplete_w = new google.maps.places.Autocomplete(input_w, options);
         };
-  };
+  }
 
 function initMap() {
   directionsDisplay = new google.maps.DirectionsRenderer();
@@ -45,7 +46,7 @@ function initMap() {
     center: city
   }
 
-  map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+  map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
   directionsDisplay.setMap(map);
   google.maps.event.addDomListener(document.getElementById('getdir'), 'click', function() {
     calculateAndDisplayRoute(directionsService, directionsDisplay);
@@ -75,6 +76,30 @@ function calculateAndDisplayRoute() {
   function(response, status) {
     if (status === 'OK') {
       directionsDisplay.setDirections(response);
+      const route = response.routes[0];
+      console.log(route.waypoint_order);
+      const summaryPanel = document.getElementById("waypoints-results");
+
+      summaryPanel.innerHTML = "<h3>Aqui está a melhor rota!</h3>";
+
+            // For each route, display summary information.
+      for (let i = 0; i < route.legs.length; i++) {
+        const routeSegment = i + 1;
+        summaryPanel.innerHTML +=
+          "<b>Parada " + routeSegment + "</b><br>";
+        if (typeof route.waypoint_order[i] !== 'undefined' && route.waypoint_order[i] !== null) {
+            let waypointName = waypts[route.waypoint_order[i]].location.split("-");
+            summaryPanel.innerHTML += waypointName[0] + "<br>";
+            summaryPanel.innerHTML += route.legs[i].end_address + "<br>";
+            summaryPanel.innerHTML += route.legs[i].distance.text + "<br><br>";
+        }
+        else {
+        summaryPanel.innerHTML += route.legs[i].end_address + "<br>";
+        summaryPanel.innerHTML += route.legs[i].distance.text + "<br><br>";
+        }
+      }
+
+      computeTotalDistance(response);
 
     } else {
       window.alert('Roteirizador falhou devido ao status: ' + status);
@@ -82,43 +107,57 @@ function calculateAndDisplayRoute() {
   });
 }
 
-var limit = 20  // Limite de waypoints, a partir de 11 o google cobra um pouco a mais e 23 é o máximo
+var limit = 20;  // Limite de waypoints, a partir de 11 o google cobra um pouco a mais e 23 é o máximo
 var counter = 1;
 var i = 0;
 
 function addWaypoint(divName){  // Cria função cujo objeto será o nome da div onde serão adicionados os elementos dinamicamente
     if (counter == limit) {
-    alert("Você chegou ao limite de " + counter + " paradas");
-  } else {
-    var div = document.createElement('div'); // cria uma div que vai conter o botão de remover e o input de texto
-    div.setAttribute("class", "waypointbox");
-    div.setAttribute("id", "box_"+counter);  // usa counter aqui para poder remover a box certa com a função remove
-    var newinput = document.createElement("input");
-    newinput.setAttribute("name", "waypoints[]");
-    newinput.setAttribute("autocomplete", "on");
-    newinput.setAttribute("type", "text");
-    newinput.setAttribute("id", "newin"+counter);
-    var remove = document.createElement("input");
-    remove.setAttribute("class", "rmvbutton");
-    remove.setAttribute("name", "remove");
-    remove.setAttribute("value", "X");
-    remove.setAttribute("type", "button");
-    remove.setAttribute("onclick", "removeWaypoint(this)");
-    remove.setAttribute("id", "remove"+counter);
-    document.getElementById(divName).appendChild(div);
-    document.getElementById("box_"+counter).appendChild(newinput);
-    document.getElementById("box_"+counter).appendChild(remove);
+        alert("Você chegou ao limite de " + counter + " paradas");
+    }
+    else {
+        var div = document.createElement('div'); // cria uma div que vai conter o botão de remover e o input de texto
+        div.setAttribute("class", "waypointbox");
+        div.setAttribute("id", "box_"+counter);  // usa counter aqui para poder remover a box certa com a função remove
+        var newinput = document.createElement("input");
+        newinput.setAttribute("name", "waypoints[]");
+        newinput.setAttribute("autocomplete", "on");
+        newinput.setAttribute("type", "text");
+        newinput.setAttribute("id", "newin"+counter);
+        newinput.setAttribute("class", "newinput");
+        var remove = document.createElement("input");
+        remove.setAttribute("class", "rmvbutton");
+        remove.setAttribute("name", "remove");
+        remove.setAttribute("value", "X");
+        remove.setAttribute("type", "button");
+        remove.setAttribute("onclick", "removeWaypoint(this)");
+        remove.setAttribute("id", "remove"+counter);
+        document.getElementById(divName).appendChild(div);
+        document.getElementById("box_"+counter).appendChild(newinput);
+        document.getElementById("box_"+counter).appendChild(remove);
 
-    counter++;
-    i++
-    console.log("cntr=" + counter + " i=" + i + " waypoints[].length=" + document.getElementsByName("waypoints[]"));
-    var autocompletew = new google.maps.places.Autocomplete(newinput, options);
+        counter++;
+        i++
+        console.log("cntr=" + counter + " i=" + i + " waypoints[].length=" + document.getElementsByName("waypoints[]"));
+        var autocompletew = new google.maps.places.Autocomplete(newinput, options);
 
   }
-};
+}
 
 function removeWaypoint(ele){
     ele.parentNode.remove();
     counter--;
     i--;
-};
+}
+
+function computeTotalDistance(result) {
+  var totalDist = 0;
+  var totalTime = 0;
+  var myroute = result.routes[0];
+  for (i = 0; i < myroute.legs.length; i++) {
+    totalDist += myroute.legs[i].distance.value;
+    totalTime += myroute.legs[i].duration.value;
+  }
+  totalDist = totalDist / 1000.
+  document.getElementById("total").innerHTML = "<b>Distância Total: </b>" + totalDist.toFixed(2) + " km<br><b>Tempo Total Aproximado: </b>" + (totalTime / 60).toFixed(0) + " minutos";
+}
