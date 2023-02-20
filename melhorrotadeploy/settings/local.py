b1cf4f104ef7f7305f97583c4a02609b457c8dbd
nycolasgh <nycolashartkopf@gmail.com>
2023-02-20 18:56:09 -0300

    commit to deploy

--- melhorrotadeploy/settings/local.py
+++ melhorrotadeploy/settings/local.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'sitealto.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv("PGDATABASE"),
+        'USER': os.getenv("PGUSER"),
+        'PASSWORD': os.getenv("PGPASSWORD"),
+        'HOST': os.getenv("PGHOST"),
+        'PORT': os.getenv("PGPORT"),
     }
 }
 
