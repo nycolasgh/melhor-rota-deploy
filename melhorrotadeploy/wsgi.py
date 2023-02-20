d74f313f20f848d1c3b1f482d214eb550966f73e
nycolasgh <nycolashartkopf@gmail.com>
2023-02-20 20:07:20 -0300

    teste 123

--- melhorrotadeploy/wsgi.py
+++ melhorrotadeploy/wsgi.py
@@ -7,8 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
-import os
-from django.core.wsgi import get_wsgi_application
+import ...
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'melhorrotadeploy.settings')
 
